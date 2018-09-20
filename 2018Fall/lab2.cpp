e326e9f71240452e82ea92bc801ae71e067ebe8c
Gerald Liu <36258909+gerald-liu@users.noreply.github.com>
2018-09-21 04:23:51 +0800

    Add files via upload

--- 2018Fall/lab2.cpp
+++ 2018Fall/lab2.cpp
@@ -10,7 +10,7 @@ public:
     void play();
 
 private:
-	bool game_over();
+    bool game_over();
     void print();
     int nim_sum();
     void player_move();
@@ -18,7 +18,7 @@ private:
 
     const int NUM_HEAPS = 3; // >= 1
     const int MAX_HEAP_SIZE = 9; // > 1
-	const char* players[2] = { "Player A", "Player B" };
+    const char* players[2] = { "Player A", "Player B" };
     vector<int> heaps;
     bool is_single_player = true;
     bool is_player_a = true;
@@ -27,8 +27,8 @@ private:
 };
 
 Nim::Nim() {
-	random_device rd;
-	default_random_engine rd_engine(rd());
+    random_device rd;
+    default_random_engine rd_engine(rd());
     uniform_int_distribution<> dist(1, MAX_HEAP_SIZE);
 
     for (int i=0; i<NUM_HEAPS; i++)
@@ -36,13 +36,13 @@ Nim::Nim() {
 }
 
 bool Nim::game_over() {
-	for (int i = 0; i < heaps.size(); i++)
-		if (heaps[i]) return false;
-	return true;
+    for (int i = 0; i < heaps.size(); i++)
+        if (heaps[i]) return false;
+    return true;
 }
 
 void Nim::print() {
-	cout << "Current State:\n";
+    cout << "Current State:\n";
     for (int i=0; i<heaps.size(); i++) 
         cout << "Heap " << i+1 << " : " << heaps[i] << "\n";
 }
@@ -57,7 +57,7 @@ int Nim::nim_sum() {
 void Nim::player_move() {
     do {
         cout << "Input the heap and the number of stones you want to remove.\n" <<
-			"(e.g. To remove 3 stones from Heap 1, input \"1 3\", separated with SPACE)\n";
+            "(e.g. To remove 3 stones from Heap 1, input \"1 3\", separated with SPACE)\n";
         cin >> heap_to_move;
         cin >> num_to_move;
     } while (heap_to_move > MAX_HEAP_SIZE || heap_to_move < 1 ||
@@ -67,14 +67,14 @@ void Nim::player_move() {
 }
 
 void Nim::ai_move() {
-	int nimsum = nim_sum();
+    int nimsum = nim_sum();
 
     // random behaviour when losing can be inevitable, waiting for the opponent to make a error
     if (nimsum == 0) {
         for (int i=0; i<heaps.size(); i++) {
             if (heaps[i] > 0) {
-				heap_to_move = i + 1;
-				num_to_move = 1;
+                heap_to_move = i + 1;
+                num_to_move = 1;
                 heaps[i] -= num_to_move;
                 return;
             }
@@ -85,8 +85,8 @@ void Nim::ai_move() {
         for (int i=0; i<heaps.size(); i++) {
             int target_size = heaps[i] ^ nimsum;
             if (target_size < heaps[i] && target_size >= 0) {
-				heap_to_move = i + 1;
-				num_to_move = heaps[i] - target_size;
+                heap_to_move = i + 1;
+                num_to_move = heaps[i] - target_size;
                 heaps[i] = target_size;
                 return;
             }
@@ -97,14 +97,14 @@ void Nim::ai_move() {
 void Nim::play() {
     do {
         cout << "You're Player A. Do you want to play with another player (0) or the AI (1)?\n" <<
-			"Input 0 or 1 as the answer.\n";
+            "Input 0 or 1 as the answer.\n";
         cin >> is_single_player;
     } while (is_single_player != 0 && is_single_player != 1);
 
-	print();
+    print();
 
     while (!game_over()) {
-		if (is_player_a) {
+        if (is_player_a) {
             cout << "You're Player A, please make your move.\n";
             player_move();
         }
@@ -116,11 +116,11 @@ void Nim::play() {
             }
         }
 
-		cout << "\n" << players[!is_player_a] << "\'s turn, remove " << num_to_move
-			<< " stones from Heap " << heap_to_move << ".\n";
+        cout << "\n" << players[!is_player_a] << "\'s turn, remove " << num_to_move
+            << " stones from Heap " << heap_to_move << ".\n";
 
         print();
-		is_player_a = !is_player_a;
+        is_player_a = !is_player_a;
     }
 
     cout << "Game Over! " << players[is_player_a] << " wins!\n";
